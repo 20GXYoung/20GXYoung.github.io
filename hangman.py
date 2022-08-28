a30734625803a651317ffae51e552ad44c575ba3
Freddy Acosta <kikeacosta3108@gmail.com>
2022-08-28 22:34:54 +0200

    estructura inicial del hangman, no tengo ni idea de que hacer

---
+++ hangman.py
@@ -0,0 +1,21 @@
+with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+    word_list = []
+    for line in f:
+        word_list.append(line.strip())
+
+def interface():
+    print("""
+    
+
+    """)
+    
+
+def hangman():
+    
+
+def run():
+    print(word_list)
+
+
+if __name__ == "__main__":
+    run()
