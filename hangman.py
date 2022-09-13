8560c1404a8b941ade59d195ccff56adfcb91bed
Freddy Acosta <kikeacosta3108@gmail.com>
2022-09-13 15:12:13 +0200

    un poco mas de progreso

--- hangman.py
+++ hangman.py
@@ -1,20 +1,26 @@
+import os
+import random
+
 with open("./archivos/data.txt", "r", encoding="utf-8") as f:
-    word_list = []
-    for line in f:
-        word_list.append(line.strip())
+    word_list = [line.strip() for line in f]
+
 
 def interface():
-    print("""
+    pass
+    
+def get_random_word():
+    word = random.choice(word_list)
+    print(word)
+    # e_word = list(enumerate(word))
     
 
-    """)
-    
 
 def hangman():
+    get_random_word()
     
 
 def run():
-    print(word_list)
+    hangman()
 
 
 if __name__ == "__main__":
